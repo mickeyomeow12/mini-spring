289c8ca00a6b13154e0db096ffa42c087e4cc38b
mickeyomeow12 <mikee8862@outlook.com>
2024-03-18 15:56:00 +0800

    bean-definition-and-bean-definition-registry

--- src/test/java/org/minispring/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
+++ src/test/java/org/minispring/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
@@ -11,10 +11,14 @@ public class BeanDefinitionAndBeanDefinitionRegistryTest {
 
 	@Test
 	public void testBeanFactory() throws Exception {
+		//创建工厂实例
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		//创建bean实例
 		BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
+		//通过这个名称获取对应的Bean实例
 		beanFactory.registerBeanDefinition("helloService", beanDefinition);
 
+		//通过beanFactory的getBean方法获取名称为helloService的Bean实例。
 		HelloService helloService = (HelloService) beanFactory.getBean("helloService");
 		helloService.sayHello();
 	}
